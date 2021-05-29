88efc2f349b9e811e5850141c2aed454ca7c8bec
zhufeng <vzhufeng@163.com>
2021-05-29 14:15:37 +0800

    use dumi

---
+++ .umirc.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'dumi';
+import pkg from './package.json';
+
+export default defineConfig({
+  title: 'weekly',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  devServer: {
+    port: pkg.port,
+  },
+  history: { type: 'hash' },
+  favicon: 'https://gw.alicdn.com/imgextra/i3/O1CN01s3UDzQ1t5LdrmxVwF_!!6000000005850-2-tps-200-200.png',
+});
